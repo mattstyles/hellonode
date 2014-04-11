57965a7089794a0b4a654bc9a783ec790b3e3ad0
Matt Styles <matt@veryfizzyjelly.com>
2014-04-11 20:52:54 +0100

    testing avow webhook

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 module.exports = http.createServer(function(req, res){
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('hello world.')
+  res.write('hello node.')
   res.end();
 });
